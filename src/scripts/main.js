91e22ec63cb78a196f1ec25105542e3f98cb10f5
Eduard Yerastov <eduardyerastov@MacBook-Pro-Eduard.local>
2019-06-11 21:54:58 +0300

    initial

---
+++ src/scripts/main.js
@@ -0,0 +1,91 @@
+import 'owl.carousel';
+import 'bootstrap/dist/js/bootstrap.min';
+
+$('.promo-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1
+});
+
+//-----
+
+var about = $('.about-slider');
+about.owlCarousel({
+    loop:true,
+    margin:10,
+    items: 1,
+    nav: false
+});
+
+//-----
+
+var servicesSlider = $('.services-slider');
+// reference for thumbnail items
+var thumbnailServicesSlider = $('.thumbnail-services-slider');
+//transition time in ms
+var duration = 500;
+
+// carousel function for main slider
+servicesSlider.owlCarousel({
+    loop:true,
+    nav:false,
+    dots: false,
+    items:1
+}).on('changed.owl.carousel', function (e) {
+    //On change of main item to trigger thumbnail item
+    thumbnailServicesSlider.trigger('to.owl.carousel', [e.item.index, duration, true]);
+});
+
+// carousel function for thumbnail slider
+thumbnailServicesSlider.owlCarousel({
+    loop:false,
+    nav:false,
+    margin: -30,
+    responsive: {
+        0: {
+            items: 1,
+            dots: false
+        },
+        480: {
+            items:5,
+            dots: true
+        }
+    }
+
+}).on('click', '.owl-item', function () {
+    // On click of thumbnail items to trigger same main item
+    servicesSlider.trigger('to.owl.carousel', [$(this).index(), duration, true]);
+
+}).on('changed.owl.carousel', function (e) {
+    // On change of thumbnail item to trigger main item
+    servicesSlider.trigger('to.owl.carousel', [e.item.index, duration, true]);
+});
+
+//------
+
+$('.team-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    items: 1
+});
+
+//------
+
+google.maps.event.addDomListener(window, 'load', init);
+function init() {
+
+    var myOptions = {
+        center: new google.maps.LatLng(48.4620111, 35.0428656), // Координаты, какое место отображать на карте
+        zoom: 16, // Уровень риближения карты
+        mapTypeId: google.maps.MapTypeId.ROADMAP // Тип карты
+
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    var marker = new google.maps.Marker({
+        position: myOptions.center,
+        map: map,
+        title: 'вуліця Троїцька, 3'
+    });
+}
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      {
+        test: /\.pug$/,
+        use: [
+          "html-loader",
+          "pug-html-loader?exports:false"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            // "style-loader", // style nodes from js strings
+            "css-loader",
+            "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {name: './assets/images/[name].[ext]'}  
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {name: './assets/fonts/[name].[ext]'}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/template/pages/index.pug",
+      filename: "./index.html"
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/template/pages/price.pug",
+      filename: "./price.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/assets/images',
+        to: './assets/images'
+      },
+      {
+        from: './src/assets/fonts',
+        to: './assets/fonts'
+      }
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
+};
